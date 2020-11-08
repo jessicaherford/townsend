88fbdd912d05500e00c416b045f7af35166e2f0a
jessicaherford <jrherford12@gmail.com>
2020-11-08 11:26:16 -0700

    few minor style changes on contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,13 +7,11 @@ function Contact() {
         <div className="row align-items-center my-5">
           <div className="col-lg-12">
             <h1 className="font-weight-light">Contact</h1>
-            <p>
-              Kent Townsend Fine Furniture is located in Salida, CO. We are 3
-              hours from Denver.
-            </p>
+            <p>Kent Townsend Fine Furniture is located in Salida, CO. </p>
+            <p>We are 3 hours from Denver.</p>
             <p>Mail: 8172 Lazy Daze Road Salida, CO 81201</p>
-            <a href="tel:719-530-1088">Tel: (719) 530-1088</a>
             <p>Email:kent@kenttownsend.com</p>
+            <a href="tel:719-530-1088">Tel: (719) 530-1088</a>
           </div>
         </div>
       </div>
