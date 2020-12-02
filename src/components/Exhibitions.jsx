17e62ae00288cb4d6e0f4bb72dca36fb8dcbebef
jessicaherford <jrherford12@gmail.com>
2020-12-02 13:03:42 -0700

    added images to Exhibitions

--- src/components/Exhibitions.jsx
+++ src/components/Exhibitions.jsx
@@ -5,13 +5,21 @@ function Exhibitions() {
     <div className="exhibitions">
       <div className="container">
         <div className="row align-items-center my-5">
-          <div className="col-lg-12">
-            <h1 className="font-weight-light">Current Galleries</h1>
-            <p>Globe Fine Art</p>
-            <p>727 Canyon Road</p>
-            <p>Santa Fe, NM 87501</p>
-            <p>Tel: 505-989-3888</p>
-            <a target="#" href="https://www.globefineart.com/">globefineart.com</a>
+          <div className="col-md-6 col-lg-6 col-sm-12">
+            <img src="./assets/globe-fine-art.jpg" alt="Salida" height="260px"></img>
+          </div>
+        <div className="col-md-6 col-lg-6 col-sm-12">
+          <h1 className="font-weight-light">Current Galleries</h1>
+          <p>Globe Fine Art</p>
+          <p>727 Canyon Road</p>
+          <p>Santa Fe, NM 87501</p>
+          <p>Tel: 505-989-3888</p>
+          <a target="#" href="https://www.globefineart.com/">globefineart.com</a>
+        </div>
+        <div className="col-md-6 col-lg-6 col-sm-12">
+          <img src="./assets/sofa.jpg"  alt="college of the redwoods" height="250px"></img>
+        </div>
+        <div className="col-md-6 col-lg-6 col-sm-12">
             <h1 className="font-weight-light">Exhibition History</h1>
             <p>SOFA Chicago, 2004 - 2012</p>
             <p>SOFA New York, 2005 - 2012</p>
